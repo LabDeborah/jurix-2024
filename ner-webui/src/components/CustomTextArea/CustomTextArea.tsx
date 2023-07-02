a650b39fd5b6641c28a5decc3226050a1fa74bff
HenriqueMiossi <henriquemiossi@gmail.com>
2023-07-02 20:42:30 -0300

    Updates

--- ner-webui/src/components/CustomTextArea/CustomTextArea.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import './CustomTextArea.css';
-
-function TextArea() {
-    return <textarea className='text-area-style' id='main-textbox' />;
-}
-
-export default TextArea;
